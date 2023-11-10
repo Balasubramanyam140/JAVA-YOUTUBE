7e3a1ebd11d293faf5e13ef00b603f77e1b7208b
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2023-11-10 18:16:22 +0530

    Create 3. Array.java
    /*
            int[] ros; //declaration of array. ros is getting defined in the stack
            ros = new int[s]; //intialisation: actually here object is being created in heap
            
            //datatype[] variableName = new datatype[size];
            
            */
            //Syntax for direct giving
            //dataType[] variableName = { Value1, value2, value3};

---
+++ 3. Array.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Main{
+    public static void main(String[] args){
+        /*
+        int[] ros; //declaration of array. ros is getting defined in the stack
+        ros = new int[s]; //intialisation: actually here object is being created in heap
+        
+        
+        */
+        //Syntax for direct giving
+        //dataType[] variableName = { Value1, value2, value3};
+        //Store 5 roll nos :
+        int[] rnos = {23,12, 45, 32, 15};
+        for(int i=0;i<rnos.length;i++){
+            System.out.print(rnos[i] + " ");
+        }
+        System.out.println();
+        
+        //Syntax 
+        //datatype[] variableName = new datatype[size];
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
+}
