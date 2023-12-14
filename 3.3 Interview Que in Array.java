0669640e6d3c7a7ceefd1739f4b17cec5eb50e30
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2023-12-14 22:39:00 +0530

    3.3 Interview Que in Array.java

--- 3.3 Interview Que in Array.java
+++ 3.3 Interview Que in Array.java
@@ -1,3 +1,39 @@
+------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------
+//Reversing an Array using Swap method
+//Two pointer method :By having two pointers
+// Swaping values in an Array
+import java.util.Scanner;
+import java.util.Arrays;
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        int[] arr = {1, 3, 23, 9 ,10};
+        reverse(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void reverse(int[] arr){
+        int start =0;
+        int end = arr.length -1;
+        
+        while(start < end){
+            swap(arr , start, end);
+            start++;
+            end--;
+        }
+    }
+    static void swap(int[] arr, int index1, int index2){
+        int temp  = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+    }
+}
+
+OUTPUT :
+        [10, 9, 23, 3, 1]
+------------------------------------------------------------------------------------------
+
 // Swaping values in an Array
 import java.util.Scanner;
 import java.util.Arrays;
