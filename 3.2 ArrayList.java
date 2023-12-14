ef81ae08773b03c3827cee9e8e5c19419f5d7b6f
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2023-12-14 20:24:38 +0530

    3.2 ArrayList.java

--- 3.2 ArrayList.java
+++ 3.2 ArrayList.java
@@ -108,3 +108,29 @@ class Main{
 OUTPUt :
         1 2 3 4 5
         1 2 3 4 5
+---------------------------------------------------------------------------------------------------
+//Multi dimentional ArrayList is also Possible
+import java.util.Scanner;
+import java.util.ArrayList;
+class HelloWorld {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        
+        //Intialization
+        for(int i=0;i<3;i++){
+            list.add(new ArrayList<>());
+        }
+        //Add elements
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                list.get(i).add(sc.nextInt());
+            }
+        }
+        System.out.println(list);
+    }
+}
+OUTPUT :
+            1 2 3 4 5 6 7 8 9
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
