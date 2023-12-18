8132c1074a8a541e1ae62ca2fb947a202af05c0c
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2023-12-18 22:31:03 +0530

    .1 Linear Search.java

--- 4.1 Linear Search.java
+++ 4.1 Linear Search.java
@@ -1,6 +1,6 @@
 Que : Searching for the target using linear Search 
 
-  
+ 
 class Main {
     public static void main(String[] args) {
         int[] nums = { 23, 45 ,28 , 17 , 28 , 30 , 19 , 26 };
@@ -22,12 +22,43 @@ class Main {
                 return i;
             }
             }
+        return -1;
             //this line will execute if none of the return statemenets above have executed
             // hence the target not found
-    
+}
+    // SEarch the target and return the element
+    static int linearSearch2(int[] arr , int target){
+        if(arr.length == 0)
+            return -1;
+        
+        // run a for loop
+        for(int i=0; i < arr.length ; i++){
+            //check for element at every index if it is = target 
+            int element = arr[i] ;
+            if(element == target){
+                return element ;
+            }
+            }
         return -1;
+            //this line will execute if none of the return statemenets above have executed
+            // hence the target not found
 }
+    // Search the target and return true or false
+    static boolean linearSearch3(int[] arr , int target){
+        if(arr.length == 0)
+            return false;
+        
+        // run a for loop
+        for(int i=0; i < arr.length ; i++){
+            //check for element at every index if it is = target 
+            int element = arr[i] ;
+            if(element == target){
+                return true ;
+            }
+            }
+        return false;
+            //this line will execute if none of the return statemenets above have executed
+            // hence the target not found
 }
-
 OUTPUT :
           6
