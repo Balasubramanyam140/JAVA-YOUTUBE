7d55b341204a633385196d742ac59d8425047a89
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2023-12-21 22:54:15 +0530

    4.3 Search_in_range.java

--- 4.3 Search_in_range.java
+++ 4.3 Search_in_range.java
@@ -1,6 +1,29 @@
-QUe : To search between range 
+Que : To search between range 
 
-  
+  class SearchInRange{
+    public static void main(String[] args){
+        int[] arr = { 10, 12, -7 ,3 ,14 , 28 };
+        int target = 14;
+        System.out.println(linearSearch(arr , target, 1, 4));
+    }
+    
+    static int linearSearch(int[] arr, int target, int start, int end){
+        if(arr.length == 0 )
+            return -1;
+        
+        for(int i= start ; i <= end ; i++){
+            int element = arr[i];
+            if(element == target)
+                return i;
+        }
+        return -1;
+    }
+}
+OUTPUT :
+          4
+ --------------------------------------------------------------------------------------------------------------
+Que : To search between range 
+
 class SearchRange{
     public static void main(String[] args){
         int[] arr = { 40 , -2 , 30, 15 , 19 };
