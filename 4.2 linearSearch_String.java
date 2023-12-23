3b785153f82cb094c72d17dce86b41294aa4d6fa
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2023-12-23 10:15:17 +0530

    4.2 linearSearch_String.java

--- 4.2 linearSearch_String.java
+++ 4.2 linearSearch_String.java
@@ -23,6 +23,36 @@ public class SearchInStrings {
 output :
         true
 --------------------------------------------------------------------------------------------------------------------------------------
+QUE : To count no of 'b' in the given word
+
+class Main{
+    public static void main(String[] args){
+        String name = "Balasubrbamanbbbyam";
+        int count =0;
+        char target ='b';
+        System.out.println(Count(name, target, count));
+        
+    }
+    static int Count(String str , char target , int count){
+            if(str.length()==0)
+                return count;
+        str = str.toLowerCase();
+        for(int i=0;i<str.length();i++){
+        
+            if(target==str.charAt(i))
+                count=count+1;
+        }
+        return count;
+    }
+}
+
+OUTPUT:
+        6
+
+--------------------------------------------------------------------------------------------------------------------------------------
+
+
+
 QUE :
     [B, a, l, a, s, u, b, r, a, m, a, n, y, a, m] -  Arrays.toString(name.toCharArray())
     1) to check character present in word or not using for loop and for each loop
