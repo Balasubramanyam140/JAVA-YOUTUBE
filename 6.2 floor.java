5daba2d2565b6d1d7e16fcd7b7611cdb5648b8b4
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2024-03-01 18:38:19 +0530

    6.2 floor.java

--- 6.2 floor.java
+++ 6.2 floor.java
@@ -3,13 +3,13 @@ class BinarySearch{
     public static void main(String[] args){
         int [] arr = { 2, 3, 5, 18, 17, 23, 48, 59, 60};
         int target = 15;
-        int ans = ceiling(arr, target);
+        int ans = floor(arr, target);
         
         System.out.println(ans);
     }
     //return the index
     //return -1 if it doesnt exist
-    static int ceiling(int[] arr, int target){
+    static int floor(int[] arr, int target){
         int start = 0;
         int end = arr.length -1;
         while(start <= end){
