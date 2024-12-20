5e3be240aa34834053fb5f21decb0519270278fd
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2024-12-20 18:54:05 +0530

    Update 08.4.2 Duplicates-array.java

--- 08.4.2 Duplicates-array.java
+++ 08.4.2 Duplicates-array.java
@@ -1,5 +1,59 @@
 QUE : Duplicates in an Array
 
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        List<Integer> duplicates = findDuplicates(arr);
+        System.out.println("Duplicates :" +duplicates );
+    }
+    
+    static List<Integer> findDuplicates(int[] arr){
+        List<Integer> result = new ArrayList<>();
+        for(int i=0;i<arr.length; i++){
+            int value = arr[i];
+            //Converting value to +ve
+            if(value <0){
+                value = -value;
+            }
+            //as like cyclic sort we r keeping it in respective index 
+            int index = value -1;
+            //if becomes true when the value is present and duplicates comes sort
+            //in else 1st value checked and change into negative so when we check again 
+            //it goes to if condition which means to list as it was duplicates this was condition we given
+            if(arr[index] < 0){
+                result.add(index+1);
+            }
+            //else comes when the value is 1st checked and then turned to -ve
+            else{
+                arr[index]= -arr[index];
+            }
+        }
+        return result;
+    }
+}
+
+INPUT :
+        6
+        3
+        4
+        3
+        5
+        3
+        2
+OUTPUT :
+        Duplicates :[3, 3]
+
+--------------------------------------------------------------------------------------------------------------
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
