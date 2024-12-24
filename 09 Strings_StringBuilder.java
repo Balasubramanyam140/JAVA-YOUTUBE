dc14ea700bcaea6e4cd4fc412da2e6005cb3b6f7
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2024-12-24 21:24:40 +0530

    Update 09 Strings_StringBuilder.java

--- 09 Strings_StringBuilder.java
+++ 09 Strings_StringBuilder.java
@@ -88,6 +88,18 @@ OUTPUT :
                 }
             }
 
+                      (or)
+                        
+            class Main{
+                    public static void main(String[] args){
+                        String series= "";
+                        for(int i=0;i<26;i++){
+                            char ch = (char)('a'+i);
+                            series = series + ch+ " ";
+                        }
+                        System.out.println(series);
+                    }
+                }
 OUTPUT :
           a b c d e f g h i j k l m n o p q r s t u v w x y z 
   -----------------------------------------------------------------------------
