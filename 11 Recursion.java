7123a11b64e74c67d803b20cfa0e47d625a2a809
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2025-01-01 21:38:43 +0530

    Update 11 Recursion.java

--- 11 Recursion.java
+++ 11 Recursion.java
@@ -1,4 +1,6 @@
 1) Ex : Write a function that prints hello world
+2) Write a function that takes in a number nd prints it
+   Print first 5 numbers: 1 2 3 4 5
 
 
 ----------------------------------------------------------------------------------------
@@ -36,4 +38,48 @@ OUTPUT :
           Hello
           Hello
           Hello
+---------------------------------------------------------------------------------------
+2) Write a function that takes in a number nd prints it
+   Print first 5 numbers: 1 2 3 4 5
+
+
+        
+                class Main{
+            public static void main(String[] args){
+                //Write a function that takes in a number nd prints it
+                //Print first 5 numbers: 1 2 3 4 5
+                print1(1);
+            }
+            static void print1(int n){
+                System.out.println(n);
+                print2(2);
+            }
+            static void print2(int n){
+                System.out.println(n);
+                print3(3);
+            }
+            static void print3(int n){
+                System.out.println(n);
+                print4(4);
+            }
+            static void print4(int n){
+                System.out.println(n);
+                print5(5);
+            }
+            static void print5(int n){
+                System.out.println(n);
+            }
+        }
+
+OUTPUT:
+        1
+        2
+        3
+        4
+        5
+
+---------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------
 ---------------------------------------------------------------------------------------
