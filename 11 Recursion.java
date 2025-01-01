461160cf4b10c1cbf91399c357d0f609517a892e
Balasubramanyam140 <108879990+Balasubramanyam140@users.noreply.github.com>
2025-01-01 21:30:46 +0530

    Create 11 Recursion.java

---
+++ 11 Recursion.java
@@ -0,0 +1,39 @@
+1) Ex : Write a function that prints hello world
+
+
+----------------------------------------------------------------------------------------
+1) Ex : Write a function that prints hello world
+        
+        class Main{
+            public static void main(String[] args){
+                //write a function that prints hello world
+                 message();
+            }
+            static void message(){
+                    System.out.println("Hello");
+                    message1();
+            }
+            static void message1(){
+                    System.out.println("Hello");
+                    message2();
+            }
+            static void message2(){
+                    System.out.println("Hello");
+                    message3();
+            }
+            static void message3(){
+                    System.out.println("Hello");
+                    message4();
+            }
+            static void message4(){
+                    System.out.println("Hello");
+            }
+        }
+
+OUTPUT :
+          Hello
+          Hello
+          Hello
+          Hello
+          Hello
+---------------------------------------------------------------------------------------
